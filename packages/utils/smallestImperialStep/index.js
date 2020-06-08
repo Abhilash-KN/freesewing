445303229e7ae1b8b4b62c69835eb7aceeac13c8
saurabh <yoursaurabh2705@gmail.com>
2020-06-08 17:16:36 +0530

    removed unnecessary .js files that were commited wrongly by devansh

--- packages/utils/smallestImperialStep/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @freesewing/utils/smallestImperialStep | v2.6.0-rc.0
- * A collection of utilities shared across freesewing frontend projects
- * (c) 2020 Joost De Cock <joost@decock.org> (https://github.com/joostdecock)
- * @license MIT
- */'use strict';const smallestImperialStep=.396875;module.exports=smallestImperialStep;
-//# sourceMappingURL=index.js.map
